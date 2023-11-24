35e7ada45459cb5099d0a38ceba290e758e49acd
Filippo Valsorda <hi@filippo.io>
2023-11-24 16:50:19 +0100

    mlkem768: add package docs and LICENSE

--- mlkem768_test.go
+++ mlkem768_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package mlkem768
 
 import (
