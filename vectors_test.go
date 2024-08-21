1b3bc76c65a115a617bf07d6eee87ca36be13426
Filippo Valsorda <hi@filippo.io>
2024-08-21 16:06:11 +0200

    mlkem768: test against ACVP vector

--- vectors_test.go
+++ vectors_test.go
@@ -2,20 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build ignore
-
 package mlkem768
 
 import (
-	"bufio"
 	"bytes"
-	"compress/gzip"
 	_ "embed"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"os"
-	"strings"
 	"testing"
 
 	"golang.org/x/crypto/sha3"
@@ -52,7 +46,9 @@ func DecapsulateFromBytes(dkBytes, c []byte) ([]byte, error) {
 		return nil, errors.New("bad key length")
 	}
 	dk := &DecapsulationKey{}
-	dk.dk = [DecapsulationKeySize]byte(dkBytes)
+	dk.ρ = [32]byte(dkBytes[len(dkBytes)-96 : len(dkBytes)-64])
+	dk.h = [32]byte(dkBytes[len(dkBytes)-64 : len(dkBytes)-32])
+	dk.z = [32]byte(dkBytes[len(dkBytes)-32:])
 	dkPKE := dkBytes[:decryptionKeySize]
 	for i := range dk.s {
 		f, err := polyByteDecode[nttElement](dkPKE[:encodingSize12])
@@ -66,9 +62,7 @@ func DecapsulateFromBytes(dkBytes, c []byte) ([]byte, error) {
 	if err := parseEK(&dk.encryptionKey, ekPKE); err != nil {
 		return nil, err
 	}
-	H := sha3.Sum256(ekPKE)
-	h := dkBytes[decryptionKeySize+encryptionKeySize : decryptionKeySize+encryptionKeySize+32]
-	if !bytes.Equal(H[:], h) {
+	if sha3.Sum256(ekPKE) != dk.h {
 		return nil, errors.New("bad ek hash")
 	}
 	return kemDecaps(dk, (*[CiphertextSize]byte)(c)), nil
@@ -82,14 +76,11 @@ func TestNISTVectors(t *testing.T) {
 		d := vector("NIST Key Generation", "d")
 		z := vector("NIST Key Generation", "z")
 		ekExp := vector("NIST Key Generation", "ek")
-		dkExp := vector("NIST Key Generation", "dk")
 
-		ek, dk := GenerateKeyDerand(t, d, z)
+		dk := kemKeyGen(nil, (*[32]byte)(d), (*[32]byte)(z))
+		ek := dk.EncapsulationKey()
 		if !bytes.Equal(ek, ekExp) {
 			t.Errorf("ek: got %x, expected %x", ek, ekExp)
-		}
-		if !bytes.Equal(dk.Bytes(), dkExp) {
-			t.Errorf("dk: got %x, expected %x", dk, dkExp)
 		}
 	})
 
@@ -124,318 +115,4 @@ func TestNISTVectors(t *testing.T) {
 			t.Errorf("k: got %x, expected %x", k, kExp)
 		}
 	})
-}
-
-func TestPQCrystalsVector(t *testing.T) {
-	d := vector("PQCrystals", "Coins")[:32]
-	z := vector("PQCrystals", "Coins")[32:]
-	ekExp := vector("PQCrystals", "Public Key")
-	dkExp := vector("PQCrystals", "Secret Key")
-
-	ek, dk := GenerateKeyDerand(t, d, z)
-	if !bytes.Equal(ek, ekExp) {
-		t.Errorf("ek: got %x, expected %x", ek, ekExp)
-	}
-	if !bytes.Equal(dk.Bytes(), dkExp) {
-		t.Errorf("dk: got %x, expected %x", dk, dkExp)
-	}
-
-	msg := vector("PQCrystals", "Message")
-	ctExp := vector("PQCrystals", "Ciphertext")
-	kExp := vector("PQCrystals", "Shared Secret")
-
-	ct, k, err := EncapsulateDerand(ek, msg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(ct, ctExp) {
-		t.Errorf("ct: got %x, expected %x", ct, ctExp)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (encaps): got %x, expected %x", k, kExp)
-	}
-
-	k, err = Decapsulate(dk, ct)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (decaps): got %x, expected %x", k, kExp)
-	}
-
-	ct = vector("PQCrystals", "Pseudorandom Ciphertext")
-	kExp = vector("PQCrystals", "Pseudorandom Shared Secret")
-
-	k, err = Decapsulate(dk, ct)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (random): got %x, expected %x", k, kExp)
-	}
-}
-
-func TestPQCKATVector(t *testing.T) {
-	d := vector("post-quantum-cryptography/KAT", "d")
-	z := vector("post-quantum-cryptography/KAT", "z")
-	ekExp := vector("post-quantum-cryptography/KAT", "pk")
-	dkExp := vector("post-quantum-cryptography/KAT", "sk")
-
-	ek, dk := GenerateKeyDerand(t, d, z)
-	if !bytes.Equal(ek, ekExp) {
-		t.Errorf("ek: got %x, expected %x", ek, ekExp)
-	}
-	if !bytes.Equal(dk.Bytes(), dkExp) {
-		t.Errorf("dk: got %x, expected %x", dk, dkExp)
-	}
-
-	msg := vector("post-quantum-cryptography/KAT", "msg")
-	ctExp := vector("post-quantum-cryptography/KAT", "ct")
-	kExp := vector("post-quantum-cryptography/KAT", "ss")
-
-	ct, k, err := EncapsulateDerand(ek, msg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(ct, ctExp) {
-		t.Errorf("ct: got %x, expected %x", ct, ctExp)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (encaps): got %x, expected %x", k, kExp)
-	}
-
-	k, err = Decapsulate(dk, ct)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (decaps): got %x, expected %x", k, kExp)
-	}
-
-	ct = vector("post-quantum-cryptography/KAT", "ct_n")
-	kExp = vector("post-quantum-cryptography/KAT", "ss_n")
-
-	k, err = Decapsulate(dk, ct)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (random): got %x, expected %x", k, kExp)
-	}
-}
-
-func TestUnluckyVector(t *testing.T) {
-	d := vector("unlucky", "d")
-	z := vector("unlucky", "z")
-	ekExp := vector("unlucky", "pk")
-	dkExp := vector("unlucky", "sk")
-
-	ek, dk := GenerateKeyDerand(t, d, z)
-	if !bytes.Equal(ek, ekExp) {
-		t.Errorf("ek: got %x, expected %x", ek, ekExp)
-	}
-	if !bytes.Equal(dk.Bytes(), dkExp) {
-		t.Errorf("dk: got %x, expected %x", dk, dkExp)
-	}
-
-	msg := vector("unlucky", "msg")
-	ctExp := vector("unlucky", "ct")
-	kExp := vector("unlucky", "ss")
-
-	ct, k, err := EncapsulateDerand(ek, msg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(ct, ctExp) {
-		t.Errorf("ct: got %x, expected %x", ct, ctExp)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (encaps): got %x, expected %x", k, kExp)
-	}
-
-	k, err = Decapsulate(dk, ct)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k (decaps): got %x, expected %x", k, kExp)
-	}
-}
-
-func TestStrcmpVector(t *testing.T) {
-	dk := vector("strcmp", "sk")
-	ct := vector("strcmp", "ct")
-	kExp := vector("strcmp", "ss")
-
-	k, err := DecapsulateFromBytes(dk, ct)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(k, kExp) {
-		t.Errorf("k: got %x, expected %x", k, kExp)
-	}
-}
-
-func TestNegativeEncaps(t *testing.T) {
-	gzipFile, err := os.Open("testdata/negative.txt.gz")
-	if err != nil {
-		t.Fatal(err)
-	}
-	gzipReader, err := gzip.NewReader(gzipFile)
-	if err != nil {
-		t.Fatal(err)
-	}
-	scanner := bufio.NewScanner(gzipReader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		ek, err := hex.DecodeString(line)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if _, _, err := Encapsulate(ek); err == nil {
-			t.Errorf("expected error for %s", line)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestWycheproofDecaps(t *testing.T) {
-	// https://groups.google.com/a/list.nist.gov/g/pqc-forum/c/aCAX-2QrUFw/m/hy5gwcESAAAJ
-	gzipFile, err := os.Open("testdata/decaps768draft.gz")
-	if err != nil {
-		t.Fatal(err)
-	}
-	gzipReader, err := gzip.NewReader(gzipFile)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var comment string
-	var dk, ct, kExp []byte
-	var expErr bool
-	scanner := bufio.NewScanner(gzipReader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			ss, err := DecapsulateFromBytes(dk, ct)
-			if err != nil && !expErr {
-				t.Errorf("unexpected error for %s: %v", comment, err)
-			}
-			if err == nil && expErr {
-				t.Errorf("expected error for %s", comment)
-			}
-			if err == nil && !bytes.Equal(ss, kExp) {
-				t.Errorf("k: got %x, expected %x", ss, kExp)
-			}
-			continue
-		}
-
-		key, value, _ := strings.Cut(line, " = ")
-		switch key {
-		case "comment":
-			comment = value
-		case "private_key":
-			dk, err = hex.DecodeString(value)
-		case "ciphertext":
-			ct, err = hex.DecodeString(value)
-		case "expected_shared_secret":
-			if value != "" {
-				kExp, err = hex.DecodeString(value)
-			} else {
-				kExp = nil
-			}
-		case "expected_result":
-			switch value {
-			case "pass":
-				expErr = false
-			case "fail":
-				expErr = true
-			default:
-				t.Fatalf("unknown expected_result %q", value)
-			}
-		default:
-			t.Fatalf("unknown key %q", key)
-		}
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestWycheproofEncaps(t *testing.T) {
-	// https://groups.google.com/a/list.nist.gov/g/pqc-forum/c/aCAX-2QrUFw/m/hy5gwcESAAAJ
-	gzipFile, err := os.Open("testdata/encaps768draft.gz")
-	if err != nil {
-		t.Fatal(err)
-	}
-	gzipReader, err := gzip.NewReader(gzipFile)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var comment string
-	var ek, m, ctExp, kExp []byte
-	var expErr bool
-	scanner := bufio.NewScanner(gzipReader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			ct, k, err := EncapsulateDerand(ek, m)
-			if err != nil && !expErr {
-				t.Errorf("unexpected error for %s: %v", comment, err)
-			}
-			if err == nil && expErr {
-				t.Errorf("expected error for %s", comment)
-			}
-			if err == nil && !bytes.Equal(ct, ctExp) {
-				t.Errorf("ct for %s: got %x, expected %x", comment, ct, ctExp)
-			}
-			if err == nil && !bytes.Equal(k, kExp) {
-				t.Errorf("k for %s: got %x, expected %x", comment, k, kExp)
-			}
-			continue
-		}
-
-		key, value, _ := strings.Cut(line, " = ")
-		switch key {
-		case "comment":
-			comment = value
-		case "public_key":
-			ek, err = hex.DecodeString(value)
-		case "entropy":
-			m, err = hex.DecodeString(value)
-		case "expected_shared_secret":
-			if value != "" {
-				kExp, err = hex.DecodeString(value)
-			} else {
-				kExp = nil
-			}
-		case "expected_ciphertext":
-			if value != "" {
-				ctExp, err = hex.DecodeString(value)
-			} else {
-				ctExp = nil
-			}
-		case "expected_result":
-			switch value {
-			case "pass":
-				expErr = false
-			case "fail":
-				expErr = true
-			default:
-				t.Fatalf("unknown expected_result %q", value)
-			}
-		default:
-			t.Fatalf("unknown key %q", key)
-		}
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		t.Fatal(err)
-	}
 }
